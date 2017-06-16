7552dde8089475ebcd286b2a7c1981ed1c186a1f
Raum Dellamorte <elementalsin@gmail.com>
2017-06-15 22:08:05 -0800

    implemented Camera, added specular lighting, started Mob support

---
+++ src/camera.rs
@@ -0,0 +1,161 @@
+
+use entities::entity::{PosMarker, Entity};
+use util::rmatrix::Matrix4f;
+use util::rvector::{RVec, Vector3f, XVEC, YVEC}; // , ZVEC
+
+pub struct Camera {
+  pub pos: Vector3f,
+  pub posBak: Vector3f,
+  pub pitch: f32,
+  pub pitchBak: f32,
+  pub yaw: f32,
+  pub yawBak: f32,
+  pub roll: f32,
+  pub rollBak: f32,
+  pub distFromFocusPos: f32,
+  pub angleAroundFocusPos: f32,
+  
+  toPos: Vector3f,
+  toFocusPos: Vector3f,
+  
+  pub viewMat: Matrix4f,
+}
+
+impl Camera {
+  pub fn create() -> Self {
+    Camera {
+      pos: Vector3f {x: 0_f32, y: 5_f32, z: 0_f32},
+      posBak: Vector3f {x: 0_f32, y: 5_f32, z: 0_f32},
+      pitch: 0_f32,
+      pitchBak: 20_f32,
+      yaw: 0_f32,
+      yawBak: 0_f32,
+      roll: 0_f32,
+      rollBak: 0_f32,
+      distFromFocusPos: 30_f32,
+      angleAroundFocusPos: 0_f32,
+      toPos: Vector3f {x: 0_f32, y: 0_f32, z: 0_f32},
+      toFocusPos: Vector3f {x: 0_f32, y: 0_f32, z: 0_f32},
+      viewMat: Matrix4f::new()
+    }
+  }
+  
+  pub fn view_matrix(&mut self) -> [[f32; 4]; 4] { self.createViewMatrix(); self.viewMat.as_slice() }
+  
+  pub fn attachListeners() {
+    //var camera = this
+    //DisplayMgr.mouse.scroll.setListener { dx: f32, dy: f32 ->
+    //  camera.distFromFocusPos -= dy * 0.5
+    //}
+  }
+  
+  pub fn store(&mut self) {
+    self.posBak.from_v3f(&self.pos);
+    self.pitchBak = self.pitch;
+    self.yawBak = self.yaw;
+    self.rollBak = self.roll;
+  }
+
+  pub fn restore(&mut self) {
+    self.pos.from_v3f(&self.posBak);
+    self.pitch = self.pitchBak;
+    self.yaw = self.yawBak;
+    self.roll = self.rollBak;
+  }
+
+  pub fn calc_pos(&mut self, follow: &PosMarker) {
+    self.calcPitch();
+    self.calcAngle();
+    self.calcCamPos(follow);
+  }
+  
+  fn calcPitch(&mut self) {
+    //if (DisplayMgr.mouse.isButtonDown(2)) self.pitch -= DisplayMgr.mouse.pos.getDY() * 0.1
+  }
+
+  fn calcAngle(&mut self) {
+    //if (DisplayMgr.mouse.isButtonDown(2)) self.angleAroundFocusPos -= DisplayMgr.mouse.pos.getDX() * 0.3
+  }
+
+  fn calcCamPos(&mut self, follow: &PosMarker) {
+    let hDist: f32 = self.calcHDistance();
+    let vDist: f32 = self.calcVDistance() + 10_f32;
+    let theta = follow.ry + self.angleAroundFocusPos;
+    let xOffset = hDist * theta.sin();
+    let zOffset = hDist * theta.cos();
+    self.pos.x = follow.pos.x - xOffset;
+    self.pos.z = follow.pos.z - zOffset;
+    self.pos.y = follow.pos.y + vDist;
+    self.yaw = 180_f32 - (follow.ry + self.angleAroundFocusPos);
+  }
+
+  fn calcHDistance(&self) -> f32 {self.distFromFocusPos * self.pitch.cos()}
+  fn calcVDistance(&self) -> f32 {self.distFromFocusPos * self.pitch.sin()}
+
+  pub fn reflection(&mut self, height: f32) {
+    self.store();
+    self.pos.y -= 2.0 * (self.pos.y - height); // y -= dist
+    self.invertPitch();
+  }
+
+  pub fn invertPitch(&mut self) {
+    self.pitch = -self.pitch;
+  }
+
+  pub fn distToPos(&mut self, vec: &Vector3f) -> f32 {
+    vec.subTo(&self.pos, &mut self.toPos);
+    self.toPos.len()
+  }
+
+  pub fn angleToEntity(&mut self, focus_pos: &Vector3f, entity: &mut Entity) -> f32 {
+    entity.distance = self.distToPos(&entity.marker.pos);
+    self.toPos.normalize();
+    focus_pos.subTo(&self.pos, &mut self.toFocusPos);
+    self.toFocusPos.normalize();
+    self.toFocusPos.dot(&self.toPos)
+  }
+  
+  pub fn createViewMatrix(&mut self) {
+    self.viewMat.setIdentity();
+    self.viewMat.rotate(self.pitch.to_radians(), &XVEC);
+    self.viewMat.rotate(self.yaw.to_radians(), &YVEC);
+    let pos = self.pos;
+    let mut negCam = Vector3f::blank();
+    pos.negateTo(&mut negCam);
+    self.viewMat.translate_v3f(&negCam);
+  }
+}
+
+pub fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+  let f = {
+    let f = direction;
+    let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+    let len = len.sqrt();
+    [f[0] / len, f[1] / len, f[2] / len]
+  };
+  
+  let s = [up[1] * f[2] - up[2] * f[1],
+           up[2] * f[0] - up[0] * f[2],
+           up[0] * f[1] - up[1] * f[0]];
+           
+  let s_norm = {
+    let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+    let len = len.sqrt();
+    [s[0] / len, s[1] / len, s[2] / len]
+  };
+  
+  let u = [f[1] * s_norm[2] - f[2] * s_norm[1],
+           f[2] * s_norm[0] - f[0] * s_norm[2],
+           f[0] * s_norm[1] - f[1] * s_norm[0]];
+           
+  let p = [-position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+           -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+           -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]];
+           
+  [
+    [s[0], u[0], f[0], 0.0],
+    [s[1], u[1], f[1], 0.0],
+    [s[2], u[2], f[2], 0.0],
+    [p[0], p[1], p[2], 1.0],
+  ]
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
+#![feature(attr_literals)]
+#![feature(custom_derive)]
+#![feature(use_extern_macros)]
 
 #[macro_use]
 extern crate nom;
 
-pub mod util;
-//use util::rvector::Vector3f;
-
-pub mod model;
-use model::import::load_obj;
-//use model::mesh::{Mesh, MeshBuffers};
-
 #[macro_use]
 extern crate glium;
+//extern crate image;
+
+pub mod camera;
+pub mod entities;
+pub mod model;
+pub mod util;
 
 fn main() {
+  use camera::Camera;
+  use model::import::load_obj;
+  use entities::entity::PosMarker;
+  //use util::rmatrix::Matrix4f;
+  //use util::rvector::Vector3f;
+  //use model::mesh::{Mesh, MeshBuffers};
+  
   use std::default::Default;
   use glium::{DisplayBuild, Program};
+  
   let display = glium::glutin::WindowBuilder::new()
     .with_title(format!("RaumEn Test"))
     .with_dimensions(1024, 760)
@@ -27,31 +37,61 @@ fn main() {
   test_nom();
   
   let vertex_shader_src = r#"
-    #version 140
-    in vec3 position;
-    in vec3 normal;
-    uniform mat4 matrix;
-    out vec3 v_normal;
-    void main() {
-      v_normal = transpose(inverse(mat3(matrix))) * normal;
-      gl_Position = matrix * vec4(position, 1.0);
-    }
-  "#;
+#version 400
+in vec3 position;
+in vec3 normal;
+
+out vec3 surface_normal;
+out vec3 v_position;
+out vec3 toLightVector;
+out vec3 toCameraVector;
+
+uniform mat4 transform;
+uniform mat4 view;
+uniform mat4 projection;
+uniform vec3 u_light;
+
+void main() {
+    v_position = gl_Position.xyz / gl_Position.w;
+    vec4 worldPos = transform * vec4(position, 1.0);
+    vec4 posRelToCam = view * worldPos;
+    gl_Position = projection * posRelToCam;
+    
+    surface_normal = (transform * vec4(normal, 0.0)).xyz;
+    
+    toLightVector = u_light - worldPos.xyz;
+    toCameraVector = (inverse(view) * vec4(0.0,0.0,0.0,1.0)).xyz - worldPos.xyz;
+}
+"#;
   let fragment_shader_src = r#"
-    #version 140
-    
-    in vec3 v_normal;
-    uniform vec3 u_light;
-    out vec4 color;
-    
-    void main() {
-      float brightness = dot(normalize(v_normal), normalize(u_light));
-      vec3 dark_color = vec3(0.6, 0.0, 0.0);
-      vec3 regular_color = vec3(1.0, 0.0, 0.0);
-      color = vec4(mix(dark_color, regular_color, brightness), 1.0);
-    }
-  "#;
+#version 400
+in vec3 surface_normal;
+in vec3 v_position;
+in vec3 toLightVector;
+in vec3 toCameraVector;
+
+out vec4 color;
+
+const vec3 ambient_color = vec3(0.2, 0.0, 0.0);
+const vec3 diffuse_color = vec3(0.6, 0.0, 0.0);
+const vec3 specular_color = vec3(1.0, 1.0, 1.0);
+
+void main() {
+  vec3 lightColour = vec3(1.0);
+  vec3 unitNormal = normalize(surface_normal);
+  vec3 unitCameraVector = normalize(toCameraVector);
+  vec3 unitLightVector = normalize(toLightVector);
   
+  float diffuse = max(dot(unitNormal, unitLightVector), 0.0);
+  float specular = max(dot(reflect(unitLightVector, unitNormal), unitCameraVector), 0.0);
+  
+  color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
+}
+"#;
+  
+  let mut cam = Camera::create();
+  let mut focus = PosMarker::new();
+  let mut pmrkr = PosMarker::new();
   let mesh = load_obj("dragon").unwrap().create_buffers(&display);
   let vb = mesh.verts;
   let ib = mesh.indcs;
@@ -60,16 +100,9 @@ fn main() {
   
   loop {
     use glium::Surface;
-    let matrix = [
-      [0.08, 0.0, 0.0, 0.0],
-      [0.0, 0.08, 0.0, 0.0],
-      [0.0, 0.0, 0.08, 0.0],
-      [0.0, 0.0, 0.0, 1.0f32]
-    ];
-    let light = [-1.0, 0.4, 0.9f32];
     
     let mut target = display.draw();
-    target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
+    target.clear_color_and_depth((0.1, 0.1, 0.1, 1.0), 1.0);
     
     let params = glium::DrawParameters {
       depth: glium::Depth {
@@ -80,13 +113,21 @@ fn main() {
       backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
       .. Default::default()
     };
-
+    
+    // Calc Movement
+    pmrkr.inc_yrot(0.01_f32);
+    
+    // Uniforms
+    let transform = pmrkr.transformation();
+    let view = { cam.calc_pos(&focus); cam.view_matrix() }; //view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
+    let projection = gen_projection(&target);
+    let light = [0.0, 1000.0, -7000.0_f32];
     
     // Draw stuff!
     target.draw(&vb,
                 &ib,
                 &program,
-                &uniform! { matrix: matrix, u_light: light },
+                &uniform! { transform: transform, view: view, projection: projection, u_light: light },
                 &params).unwrap();
     
     target.finish().unwrap();
@@ -98,4 +139,25 @@ fn main() {
       }
     }
   }
+}
+
+fn gen_projection(target: &glium::Frame) -> [[f32; 4]; 4] {
+  use glium::Surface;
+  use util::rmatrix::Matrix4f;
+  let (width, height) = target.get_dimensions();
+  let aspect_ratio = height as f32 / width as f32;
+  
+  let fov: f32 = 3.141592 / 3.0;
+  let zfar = 1024.0;
+  let znear = 0.1;
+  let mut pMatrix = Matrix4f::new();
+  let yScale = 1_f32 / (fov / 2_f32).tan();
+  let frustumLength = zfar - znear;
+  pMatrix.m00 = yScale / aspect_ratio;
+  pMatrix.m11 = yScale;
+  pMatrix.m22 = -((zfar + znear) / frustumLength);
+  pMatrix.m23 = -1_f32;
+  pMatrix.m32 = -(2_f32 * znear * zfar) / frustumLength;
+  pMatrix.m33 = 0_f32;
+  pMatrix.as_slice()
 }
