83e87e0123037273b70926c3320c39921a5ece5d
Raum Dellamorte <elementalsin@gmail.com>
2017-06-18 22:08:56 -0800

    Much better input handling

--- src/entities/mobs.rs
+++ src/entities/mobs.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 use entities::entity::Entity;
 use input::Handler;
+use glutin::VirtualKeyCode::*;
+use glutin::MouseButton;
 
 pub struct Mob<'a> {
   pub entity: Entity,
@@ -18,11 +20,16 @@ impl<'a> Mob<'a> {
   }
   
   pub fn move_mob(&mut self, handler: &mut Handler, rate: f32) -> &Self {
-    if handler.read_multi("W") { self.entity.marker.pos.z += self.speed * rate; } // Up
-    if handler.read_multi("A") { self.entity.marker.pos.x += self.speed * rate; } // Left
-    if handler.read_multi("S") { self.entity.marker.pos.z -= self.speed * rate; } // Down
-    if handler.read_multi("D") { self.entity.marker.pos.x -= self.speed * rate; } // Right
-    if handler.read_single("Space") {} // Jump
+    let (mx, my) = match handler.cursor_pos {
+      Some(xy) => xy,
+      None     => (0_i32, 0_i32),
+    };
+    if handler.read_kb_multi(W) { self.entity.marker.pos.z += self.speed * rate; } // Up
+    if handler.read_kb_multi(A) { self.entity.marker.pos.x += self.speed * rate; } // Left
+    if handler.read_kb_multi(S) { self.entity.marker.pos.z -= self.speed * rate; } // Down
+    if handler.read_kb_multi(D) { self.entity.marker.pos.x -= self.speed * rate; } // Right
+    if handler.read_kb_single(Space) {} // Jump
+    if handler.read_mouse_single(MouseButton::Left) { println!("mouse x: {} y: {}", mx, my); } // Fire/Select
     self
   }
 }
--- src/input.rs
+++ src/input.rs
@@ -3,56 +3,54 @@ use glutin::Event;
 use glutin::Event::KeyboardInput as KB;
 use glutin::Event::MouseInput as Click;
 use glutin::ElementState::{Pressed, Released};
-use glutin::VirtualKeyCode::*;
+use glutin::VirtualKeyCode;
 use glutin::MouseButton;
 
-pub struct Handler<'a> {
-  pub map: HashMap<&'a str, bool>,
-  pub mouse_other: HashMap<u8, bool>,
+pub struct Handler {
+  pub kb: HashMap<VirtualKeyCode, bool>,
+  pub mouse: HashMap<MouseButton, bool>,
   pub cursor_pos: Option<(i32, i32)>,
 }
 
-impl<'a> Handler<'a> {
+impl Handler {
   pub fn new() -> Self {
-    Handler { map: HashMap::new(), mouse_other: HashMap::new(), cursor_pos: None }
+    Handler { kb: HashMap::new(), mouse: HashMap::new(), cursor_pos: None }
   }
   
   pub fn event(&mut self, event: &Event) {
     match event {
-      &Event::MouseMoved(x,y) => { self.cursor_pos = Some((x,y)) },
-      &Click(Pressed,  MouseButton::Left)     => { self.map.insert("ClickL", true); },  // Pressed Left Mouse
-      &Click(Released, MouseButton::Left)     => { self.map.insert("ClickL", false); }, // Released Left Mouse
-      &Click(Pressed,  MouseButton::Right)    => { self.map.insert("ClickR", true); },  // Pressed Right Mouse
-      &Click(Released, MouseButton::Right)    => { self.map.insert("ClickR", false); }, // Released Right Mouse
-      &Click(Pressed,  MouseButton::Middle)   => { self.map.insert("ClickM", true); },  // Pressed Right Mouse
-      &Click(Released, MouseButton::Middle)   => { self.map.insert("ClickM", false); }, // Released Right Mouse
-      &Click(Pressed,  MouseButton::Other(x)) => { self.mouse_other.insert(x, true); }, // Pressed Right Mouse
-      &Click(Released, MouseButton::Other(x)) => { self.mouse_other.insert(x, false); },// Released Right Mouse
-      &KB(Pressed,  _, Some(Space)) => { self.map.insert("Space", true); },  // Pressed Space
-      &KB(Released, _, Some(Space)) => { self.map.insert("Space", false); }, // Released Space
-      &KB(Pressed,  _, Some(Down))  => { self.map.insert("Down", true); },  // Pressed Down
-      &KB(Released, _, Some(Down))  => { self.map.insert("Down", false); }, // Released Down
-      &KB(Pressed,  _, Some(A)) => { self.map.insert("A", true); },  // Pressed A
-      &KB(Released, _, Some(A)) => { self.map.insert("A", false); }, // Released A
-      &KB(Pressed,  _, Some(D)) => { self.map.insert("D", true); },  // Pressed D
-      &KB(Released, _, Some(D)) => { self.map.insert("D", false); }, // Released D
-      &KB(Pressed,  _, Some(W)) => { self.map.insert("W", true); },  // Pressed W
-      &KB(Released, _, Some(W)) => { self.map.insert("W", false); }, // Released W
-      &KB(Pressed,  _, Some(S)) => { self.map.insert("S", true); },  // Pressed S
-      &KB(Released, _, Some(S)) => { self.map.insert("S", false); }, // Released S
+      &Event::MouseMoved(x,y)      => { self.cursor_pos = Some((x,y)) },
+      &Click(Pressed,  mbttn)      => { self.mouse.insert(mbttn, true); },
+      &Click(Released, mbttn)      => { self.mouse.insert(mbttn, false); },
+      &KB(Pressed,  _, Some(bttn)) => { self.kb.insert(bttn, true); },
+      &KB(Released, _, Some(bttn)) => { self.kb.insert(bttn, false); },
       _ => {}
     }
   }
   
-  pub fn read_single(&mut self, key: &'a str) -> bool {
-    match self.map.insert(key, false) {
+  pub fn read_kb_single(&mut self, key: VirtualKeyCode) -> bool {
+    match self.kb.insert(key, false) {
       Some(tf) => { return tf; },
       None     => { return false; },
     }
   }
   
-  pub fn read_multi(&self, key: &'a str) -> bool {
-    match self.map.get(key) {
+  pub fn read_kb_multi(&self, key: VirtualKeyCode) -> bool {
+    match self.kb.get(&key) {
+      Some(&tf) => { return tf; },
+      None      => { return false; },
+    }
+  }
+  
+  pub fn read_mouse_single(&mut self, key: MouseButton) -> bool {
+    match self.mouse.insert(key, false) {
+      Some(tf) => { return tf; },
+      None     => { return false; },
+    }
+  }
+  
+  pub fn read_mouse_multi(&self, key: MouseButton) -> bool {
+    match self.mouse.get(&key) {
       Some(&tf) => { return tf; },
       None      => { return false; },
     }
