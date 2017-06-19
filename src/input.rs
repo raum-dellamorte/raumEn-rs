bb5e27e2e241e81ae8cb392ef41244d1d5fbdfd8
Raum Dellamorte <elementalsin@gmail.com>
2017-06-18 21:05:30 -0800

    mouse button support

--- src/input.rs
+++ src/input.rs
@@ -1,22 +1,33 @@
 use std::collections::HashMap;
 use glutin::Event;
 use glutin::Event::KeyboardInput as KB;
+use glutin::Event::MouseInput as Click;
 use glutin::ElementState::{Pressed, Released};
 use glutin::VirtualKeyCode::*;
+use glutin::MouseButton;
 
 pub struct Handler<'a> {
   pub map: HashMap<&'a str, bool>,
+  pub mouse_other: HashMap<u8, bool>,
   pub cursor_pos: Option<(i32, i32)>,
 }
 
 impl<'a> Handler<'a> {
   pub fn new() -> Self {
-    Handler { map: HashMap::new(), cursor_pos: None }
+    Handler { map: HashMap::new(), mouse_other: HashMap::new(), cursor_pos: None }
   }
   
   pub fn event(&mut self, event: &Event) {
     match event {
       &Event::MouseMoved(x,y) => { self.cursor_pos = Some((x,y)) },
+      &Click(Pressed,  MouseButton::Left)     => { self.map.insert("ClickL", true); },  // Pressed Left Mouse
+      &Click(Released, MouseButton::Left)     => { self.map.insert("ClickL", false); }, // Released Left Mouse
+      &Click(Pressed,  MouseButton::Right)    => { self.map.insert("ClickR", true); },  // Pressed Right Mouse
+      &Click(Released, MouseButton::Right)    => { self.map.insert("ClickR", false); }, // Released Right Mouse
+      &Click(Pressed,  MouseButton::Middle)   => { self.map.insert("ClickM", true); },  // Pressed Right Mouse
+      &Click(Released, MouseButton::Middle)   => { self.map.insert("ClickM", false); }, // Released Right Mouse
+      &Click(Pressed,  MouseButton::Other(x)) => { self.mouse_other.insert(x, true); }, // Pressed Right Mouse
+      &Click(Released, MouseButton::Other(x)) => { self.mouse_other.insert(x, false); },// Released Right Mouse
       &KB(Pressed,  _, Some(Space)) => { self.map.insert("Space", true); },  // Pressed Space
       &KB(Released, _, Some(Space)) => { self.map.insert("Space", false); }, // Released Space
       &KB(Pressed,  _, Some(Down))  => { self.map.insert("Down", true); },  // Pressed Down
