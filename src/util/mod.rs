2ea742cb0286b90a9724b824eab1fad440abb3f9
Raum Dellamorte <elementalsin@gmail.com>
2018-08-29 23:15:21 -0800

    ease of access

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,6 +3,9 @@ pub mod rvector;
 pub mod rmatrix;
 pub mod rvertex;
 
+pub use util::rmatrix::Matrix4f;
+pub use util::rvector::{Vector2f, Vector3f};
+
 // #[derive(Debug, Copy, Clone)]
 // pub struct TransMat {
 //   pub transform: [f32; 16],
