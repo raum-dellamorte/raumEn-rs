00e3d91bad8525151db72d55eb5eff347f2b5b22
raum-dellamorte <raum-dellamorte@users.noreply.github.com>
2019-02-20 00:16:17 -0900

    particle prep

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -2,9 +2,13 @@
 pub mod entity;
 pub mod entitymgr;
 pub mod mobs;
+pub mod particle;
+pub mod particlemgr;
 pub mod position;
 
 pub use entities::entity::*;
 pub use entities::entitymgr::*;
 pub use entities::mobs::Mob;
+pub use entities::particle::*;
+pub use entities::particlemgr::*;
 pub use entities::position::PosMarker;
