904e9ed96cd8cdaed45a6d39f2f019a1b88c2460
Raum Dellamorte <elementalsin@gmail.com>
2019-10-11 22:14:25 -0800

    Particle System prep

---
+++ src/ecs/c/particle.rs
@@ -0,0 +1,17 @@
+
+
+use {
+  specs::{
+    Component, VecStorage,
+  },
+  util::{
+    Vector3f,
+    HashSet,
+  },
+};
+
+#[derive(Component, Debug)]
+#[storage(VecStorage)]
+pub struct Particle {
+  
+}
---
+++ src/ecs/s/particle.rs
@@ -0,0 +1,27 @@
+
+use {
+  specs::{
+    Entities, System, Read, Write, ReadStorage, WriteStorage, Join, 
+  },
+  ecs::{
+    c::{
+      position::{
+        *,
+      },
+      terrain::{
+        Platform,
+        TerrainNodes,
+      },
+    },
+    // s::{
+      
+    // },
+  },
+  flags::*,
+  // util::{
+  //   Vector3f, 
+  //   modulo,
+  // },
+};
+
+
